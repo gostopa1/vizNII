710ecaee10f01ddf20b9a01293bc22e5b9fe8b41
gostopa1 <tanilas@hotmail.com>
2016-06-16 13:43:35 +0300

    Update transform 20to05
    Added some extra info on what I am doing and why because it was a bit of a mess there.

--- transform20to05.m
+++ transform20to05.m
@@ -11,15 +11,13 @@ transmat=[curdir '/transformation_matrices/' '2mmto05mm.mat'];
 system(['flirt -in ' filename ' -applyxfm -init ' transmat ' -out ' filename_hires  ' -paddingsize 0.0 -interp trilinear -ref ' ref])
 system(['gzip -d ' filename_hires '.gz' ])
 
-%%
-dir  MNI152_T1_2mm_brain.nii
-
+%% This part is to fix the originator if needed. So it gets the originator from the 2mm template. 
 
 niitemp=load_nii(filename);
 niibase=load_nii('MNI152_T1_2mm_brain.nii');
 
 niitemp.hdr=niibase.hdr;
 
-niitemp.img=niitemp.img*10000;
+niitemp.img=niitemp.img*10000; % Beware that this causes scaling of the data (It might produce p-values>1000!) It is done just because high bit resolution is required for low decimal values which leads to big NIFTI files
 
 save_nii(niitemp,'test2mm2.nii')
